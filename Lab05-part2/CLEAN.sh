662706aac3697255968f9f53de8b9eb80953c868
gihanjayatilaka <gihanchanaka@gmail.com>
2020-05-08 02:24:05 +0530

    minor adjustment

---
+++ Lab05-part2/CLEAN.sh
@@ -0,0 +1,11 @@
+cd groundtruth/
+rm compiled.out
+rm waveDump.vcd
+rm printOutputs.txt
+cd ../
+rm -r temp1/
+rm -r temp2/
+rm -r temp3/
+rm gen-bash
+rm tempscript.sh
+rm gen-marks
--- Lab05-part2/MARK.sh
+++ Lab05-part2/MARK.sh
@@ -1,32 +1,21 @@
 echo "CO224 Register File Lab Marking Script"
 echo "-----------------------------------------------"
+chmod 700 CLEAN.sh
+./CLEAN.sh
 cd groundtruth/
-rm compiled.out
-rm waveDump.vcd
-rm printOutputs.txt
 iverilog *.v -o compiled.out -s reg_file_tb_groundtruth
 ./compiled.out > printOutputs.txt
 cd ../
 
-rm -r temp1/
 mkdir temp1/
 unzip $1 -d temp1/
 
-rm -r temp2/
 mkdir temp2/
 cp temp1/E*/*.zip temp2/
 
-rm -r temp3/
 mkdir temp3/
 
-# rm -r temp4/
-# mkdir temp4/
 
-# rm -r temp5/
-# mkdir temp5/
-
-rm gen-bash
-rm tempscript.sh
 g++ gen-bash.cpp -o gen-bash -lboost_filesystem -lboost_system
 ./gen-bash > tempscript.sh
 
@@ -34,7 +23,6 @@ chmod 700 tempscript.sh
 rm log.txt
 ./tempscript.sh > log.txt
 
-rm gen-marks
 rm MARKS_LIST.csv
 g++ gen-marks.cpp -o gen-marks -lboost_filesystem -lboost_system
 ./gen-marks > MARKS_LIST.csv
